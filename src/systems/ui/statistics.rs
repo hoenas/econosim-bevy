eae7b38af417878df131edc2956e34fbb73469a5
hoe nas <hoenas@gmail.com>
2025-11-04 22:17:59 +0100

    Fixed compile

--- src/systems/ui/statistics.rs
+++ src/systems/ui/statistics.rs
@@ -2,6 +2,6 @@ use bevy::prelude::*;
 use bevy_egui::{EguiContexts, egui};
 
 pub fn graph_system(mut contexts: EguiContexts) -> Result {
-    egui::Window::new("Graph").show(contexts.ctx_mut()?, |ui| ui.image(source));
+    egui::Window::new("Graph").show(contexts.ctx_mut()?, |ui| ui.label("Graph"));
     Ok(())
 }
