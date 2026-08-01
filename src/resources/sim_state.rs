5e414ba5668e6534f262991165de915419ac794d
joenas <hoenas@gmail.com>
2026-08-01 12:56:14 +0200

    Start the simulation in paused mode

--- src/resources/sim_state.rs
+++ src/resources/sim_state.rs
@@ -1,8 +1,14 @@
 use bevy::prelude::*;
 
-#[derive(Resource, Default)]
+#[derive(Resource)]
 pub struct SimState {
     pub paused: bool,
     pub step_requested: bool,
     pub reset_requested: bool,
+}
+
+impl Default for SimState {
+    fn default() -> Self {
+        Self { paused: true, step_requested: false, reset_requested: false }
+    }
 }
